68d72b65857bbdd3230c0245064cb265c72057be
shahrooznld <shahrooz.nld@gmail.com>
2019-09-05 11:04:08 +0200

    make register disabled

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,15 @@ Route::resource('{exam}/questions', 'QuestionController');
 Route::resource('users', 'UserController');
 Route::resource('exams', 'ExamController');
 Route::resource('sends', 'SendToAllController');
-Auth::routes();
+
+// Auth::routes();
+
+///make disable registeratuo
+Auth::routes([
+  'register' => false, // Registration Routes...
+  'reset' => false, // Password Reset Routes...
+  'verify' => false, // Email Verification Routes...
+]);
+
 
 Route::get('/home', 'HomeController@index')->name('home');
